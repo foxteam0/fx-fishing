1c763336e51cf4970ed6d28b112a2523567b21e2
FOXTEAM <133250817+foxteam0@users.noreply.github.com>
2023-12-19 17:04:22 +0300

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,62 +1,49 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-
 QBCore.Functions.CreateUseableItem('fishingrod', function(source, item)
-	local Player = QBCore.Functions.GetPlayer(source)
-	if not Player.Functions.GetItemByName(item.name) then return end
+    local Player = QBCore.Functions.GetPlayer(source)
+    if not Player.Functions.GetItemByName(item.name) then return end
 
-    TriggerClientEvent('itemkontrol',source)
+    TriggerClientEvent('itemkontrol', source)
 end)
 
+RegisterNetEvent('balikitemver')
+AddEventHandler('balikitemver', function(itemname)
+    local src = source
+    local xPlayer = QBCore.Functions.GetPlayer(src)
 
-RegisterNetEvent('balikitemver',function(itemname)
-src=source
-local xPlayer = QBCore.Functions.GetPlayer(src)
-		
-amount = 1
-	local random = math.random(1,5) 
-	xPlayer.Functions.RemoveItem('fishbait', random)
+    local random = math.random(1, 5)
+    xPlayer.Functions.RemoveItem('fishbait', random)
 
-	TriggerClientEvent("QBCore:Notify",source, "Balıklar Yemi Yedi", "error", 2500)
+    TriggerClientEvent("QBCore:Notify", source, "Balıklar Yemi Yedi", "success", 2500)
 
-
-
-xPlayer.Functions.AddItem(itemname,1)
-TriggerClientEvent('inventory:client:ItemBox',source, QBCore.Shared.Items[itemname], 'add')
-
+    xPlayer.Functions.AddItem(itemname, 1)
+    TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items[itemname], 'add')
 end)
 
-local GiveDirty = true
-
-Shellitem = {
+local Shellitem = {
     ['codfish'] = 100,
-
+    -- Diğer balık türlerini ekleyebilirsiniz.
 }
 
 RegisterServerEvent('sellfish')
 AddEventHandler('sellfish', function(itemName, amount)
+    local xPlayer = QBCore.Functions.GetPlayer(source)
+    local price = Shellitem[itemName]
 
-	local xPlayer = QBCore.Functions.GetPlayer(source)
-	local price = Shellitem['codfish']
-	local xItem = QBCore.Functions.HasItem(source,'codfish',Config.amount)
-	if not price then
-		return
-	end
-if xItem then
---	xPlayer.Functions.AddMoney('cash', price, "Bank depost") -- teker teker satma yapmak için açın
-	xPlayer.Functions.AddMoney('cash', price * xItem.amount , "Bank depost") -- banka
+    if not price then
+        return
+    end
 
-	TriggerClientEvent("QBCore:Notify",source, "Güzel iş biraz daha getir.", "success", 2500)
-	xPlayer.Functions.RemoveItem('codfish', xItem.amount)
---xPlayer.Functions.RemoveItem('codfish', amount) teker teker satma yapmak için açın
+    local xItem = xPlayer.Functions.GetItemByName(itemName)
 
-else
+    if xItem and xItem.amount >= amount then
+        local totalAmount = price * amount
+        xPlayer.Functions.AddMoney('cash', totalAmount, "Fish Sale")
+        xPlayer.Functions.RemoveItem(itemName, amount)
 
-	TriggerClientEvent("QBCore:Notify",source, "Yeterince Malzeme Yok", "error", 2500)
-
-
-
-
-end
-	
+        TriggerClientEvent("QBCore:Notify", source, "Satış başarılı, $" .. totalAmount .. " kazandın.", "success", 2500)
+    else
+        TriggerClientEvent("QBCore:Notify", source, "Yeterince malzeme yok", "error", 2500)
+    end
 end)
